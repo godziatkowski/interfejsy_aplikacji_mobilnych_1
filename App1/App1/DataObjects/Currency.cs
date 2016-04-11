1421fffef22b347a82b5cc04ef8b99a4d35c896f
Jan Godziątowski <ph.godzio@gmail.com>
2016-04-11 23:37:01 +0200

    Create Data Object to store information about currency

---
+++ App1/App1/DataObjects/Currency.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App1.DataObjects
+{
+    public class Currency
+    {
+        public String currencyName { get; set; }
+        public int conversionRate { get; set; }        
+        public String currencyCode { get; set; }        
+        public double currencyAsPLN { get; set; }
+
+    }
+}
